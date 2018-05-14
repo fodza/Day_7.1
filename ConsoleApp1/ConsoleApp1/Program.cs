d4c5e62f700b4b62e512cb422b0d8bae5ef9c31d
Agnese <fodza@inbox.lv>
2018-05-14 19:42:08 +0300

    Day_7majas darbs
    3.Majas darbs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ReizTabula();
+            Console.ReadLine();
+        }
+
+        static void ReizTabula()
+
+        {
+            Console.WriteLine("Ievadiet skaitli 1 ");
+            string ievade = Console.ReadLine();
+            int skaitlis = Convert.ToInt16(ievade);
+
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i );
+
+            }
+            
+        }
+
+    }
+}
